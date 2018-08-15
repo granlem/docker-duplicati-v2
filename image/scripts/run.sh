039da2bc9c9d012ddfa8bb6bb1bb0059a82e6177
granlem <kontak@maximeveit.de>
2018-08-15 22:25:16 +0200

    Fixed binding of any interface and generate password if no one is provided

--- image/scripts/run.sh
+++ image/scripts/run.sh
@@ -3,5 +3,12 @@
 DATA_FOLDER=${DATA_FOLDER:-"/data"}
 UPLOAD_FOLDER=${UPLOAD_FOLDER:-"/upload"}
 WEBSERVICE_PORT=${WEBSERVICE_PORT:-8200}
+ALLOW_NO_PASSWORD=${ALLOW_NO_PASSWORD:-false}
 
-duplicati-server --server-datafolder="${DATA_FOLDER}" --asynchronous-upload-folder="${UPLOAD_FOLDER}" --webservice-port="${WEBSERVICE_PORT}" --webservice-password=${WEBSERVICE_PASSWORD}
+# If no "WEBSERVICE_PASSWORD" is not set than create a password (security-by-default)
+if [ -z $WEBSERVICE_PASSWORD ] && [ $ALLOW_NO_PASSWORD = true ]; then
+    WEBSERVICE_PASSWORD=$(< /dev/urandom tr -dc _A-Z-a-z-0-9 | head -c${1:-16};echo;)
+    echo "The generated web frontent password is ${WEBSERVICE_PASSWORD}"
+fi
+
+duplicati-server --webservice-interface=any --server-datafolder="${DATA_FOLDER}" --asynchronous-upload-folder="${UPLOAD_FOLDER}" --webservice-port="${WEBSERVICE_PORT}" --webservice-password="${WEBSERVICE_PASSWORD}"
